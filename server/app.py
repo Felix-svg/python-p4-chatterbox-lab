59470c04bbf3cb234b2ea944fc9f112b794222db
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-04-09 19:43:14 +0300

    Complete assignment

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = []
+        for message in Message.query.order_by(Message.created_at).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response = make_response(messages, 200)
+        return response
+    elif request.method == "POST":
+        body = request.json.get("body")
+        username = request.json.get("username")
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
 
-@app.route('/messages/<int:id>')
+        # new_message = Message(
+        #     body=request.form.get("body"),
+        #     username=request.form.get("username"),
+        # )
+        # db.session.add(new_message)
+        # db.session.commit()
+        # message_dict = new_message.to_dict()
+        # return jsonify(message_dict), 201
+        # # response = make_response(message_dict, 201)
+        # # return response
+
+
+@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == "PATCH":
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+        db.session.add(message)
+        db.session.commit()
+        message_dict = message.to_dict()
+        response = make_response(message_dict, 200)
+        return response
 
-if __name__ == '__main__':
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {"delete_successful": True, "message": "Message deleted"}
+
+
+if __name__ == "__main__":
     app.run(port=5555)
